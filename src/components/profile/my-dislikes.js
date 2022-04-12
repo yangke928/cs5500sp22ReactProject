dff6dc1eeacee82d3fb1ff357bdd549822b95959
yangke <yang.ke2@husky.neu.edu>
2022-04-11 22:55:43 -0400

    new feature of my dislike screen

--- src/components/profile/my-dislikes.js
+++ src/components/profile/my-dislikes.js
@@ -3,5 +3,16 @@ import * as service from "../../services/dislike-service";
 import {useEffect, useState} from "react";
 
 const MyDislikes = () => {
+    const [dislikedTuits, setDislikedTuits] = useState([]);
+    const findTuitsDislike = () =>
+        service.findAllTuitsDislikedByUser("me")
+            .then((tuits) => setDislikedTuits(tuits));
+    useEffect(findTuitsDislike,[]);
+    return(
+        <div>
+            <h1>My Dislikes</h1>
+            <Tuits tuits={dislikedTuits} refreshTuits={findTuitsDislike}/>
+        </div>
+    )
 }
 export default MyDislikes;
